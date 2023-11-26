b384fec1ebecdecb65708d89609ecfa59672cc04
Ameer <ameerapex496@gmail.com>
2023-11-26 19:48:51 +0100

    added style and functions to the project

--- src/script.js
+++ src/script.js
@@ -1,1 +1,48 @@
-console.log("hallo world");
+var smallCups = document.querySelectorAll(".cup-small");
+var liters = document.getElementById("liters");
+var percentage = document.getElementById("percentage");
+var remained = document.getElementById("remained");
+updateBigCup();
+smallCups.forEach(function (cup, idx) {
+    cup.addEventListener("click", function () { return highlightCups(idx); });
+});
+function highlightCups(idx) {
+    var _a;
+    if (idx === 7 && smallCups[idx].classList.contains("full")) {
+        idx--;
+    }
+    else if (smallCups[idx].classList.contains("full") &&
+        !((_a = smallCups[idx].nextElementSibling) === null || _a === void 0 ? void 0 : _a.classList.contains("full"))) {
+        idx--;
+    }
+    smallCups.forEach(function (cup, idx2) {
+        if (idx2 <= idx) {
+            cup.classList.add("full");
+        }
+        else {
+            cup.classList.remove("full");
+        }
+    });
+    updateBigCup();
+}
+function updateBigCup() {
+    var fullCups = document.querySelectorAll(".cup-small.full").length;
+    var totalCups = smallCups.length;
+    if (fullCups === 0) {
+        percentage.style.visibility = "hidden";
+        percentage.style.height = "0";
+    }
+    else {
+        percentage.style.visibility = "visible";
+        percentage.style.height = "".concat((fullCups / totalCups) * 330, "px");
+        percentage.innerText = "".concat((fullCups / totalCups) * 100, "%");
+    }
+    if (fullCups === totalCups) {
+        remained.style.visibility = "hidden";
+        remained.style.height = "0";
+    }
+    else {
+        remained.style.visibility = "visible";
+        liters.innerText = "".concat(2 - (250 * fullCups) / 1000, "L");
+    }
+}
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,55 @@
-console.log("hallo world");
+const smallCups = document.querySelectorAll(
+  ".cup-small"
+) as NodeListOf<HTMLElement>;
+const liters = document.getElementById("liters") as HTMLElement;
+const percentage = document.getElementById("percentage") as HTMLElement;
+const remained = document.getElementById("remained") as HTMLElement;
+
+updateBigCup();
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => highlightCups(idx));
+});
+
+function highlightCups(idx: number) {
+  if (idx === 7 && smallCups[idx].classList.contains("full")) {
+    idx--;
+  } else if (
+    smallCups[idx].classList.contains("full") &&
+    !smallCups[idx].nextElementSibling?.classList.contains("full")
+  ) {
+    idx--;
+  }
+
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = "0";
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = "0";
+  } else {
+    remained.style.visibility = "visible";
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
+}
